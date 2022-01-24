6058aaec08de788dfc54366afb466a9fc4d84350
dpsmnsk <dpsmnsk@gmail.com>
2022-01-24 21:31:56 +0500

    work in progress

---
+++ models/productModel.js
@@ -0,0 +1,29 @@
+const products = require('../data/products.json');
+const { v4: uuid4 } = require('uuid');
+
+const { writeDataToFile } = require('../utils');
+
+function findAll() {
+    return new Promise((resolve, reject) => {
+        resolve(products);
+    })
+};
+
+function findById(id) {
+    return new Promise((resolve, reject) => {
+        const product = products.find((p) => p.id === id);
+        resolve(product);
+    })
+};
+
+function create(product) {
+    return new Promise((resolve, reject) => {
+        const newProduct = {id: uuid4(), ...product};
+        products.push(newProduct);
+        writeDataToFile('./data/products.json', products);
+        resolve(newProduct);
+    })
+};
+
+
+module.exports = { findAll, findById, create };
